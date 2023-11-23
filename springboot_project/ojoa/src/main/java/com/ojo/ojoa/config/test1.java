611df3c12077502d6ba5f88530e924d27ea5d818
어성은 <djandcl@naver.com>
2023-11-23 15:55:10 +0900

    스프링부트프로젝트 생성

---
+++ springboot_project/ojoa/src/main/java/com/ojo/ojoa/config/test1.java
@@ -0,0 +1,13 @@
+package com.ojo.ojoa.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class test1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(test1.class, args);
+	}
+
+}
