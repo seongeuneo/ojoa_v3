332ce999e7d145b50b62eb552448ed8ed0c51c11
DESKTOP-PARGD12\Wonhee <lehn3599@naver.com>
2023-11-27 11:07:54 +0900

    Cart / Order 파트 수정한거!

--- springboot_project/ojoa/src/main/java/com/ojo/ojoa/controller/OrdersController.java
+++ springboot_project/ojoa/src/main/java/com/ojo/ojoa/controller/OrdersController.java
@@ -1,47 +1,61 @@
 package com.ojo.ojoa.controller;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ojo.ojoa.entity.Orders;
 import com.ojo.ojoa.service.OrdersService;
 
-@RestController
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+
+@AllArgsConstructor
+@Controller
+@Log4j2
 @RequestMapping("/orders")
 public class OrdersController {
-	private final OrdersService ordersService;
 
-	public OrdersController(OrdersService orderService) {
-		this.ordersService = orderService;
-	}
-	
-	// 주문 조회 
-	@GetMapping("/{id}") 
-	public Orders getOrderById(@PathVariable Long id) {
-		// OrderService를 통해 해당 id의 주문을 가져와 반환
-		return ordersService.getOrderById(id);
-	}
-	
-	// 주문 생성 
-	@PostMapping("/create")
-	public Orders createOrder(@RequestBody Orders order) {
-		// 주문을 생성하고 반환
-		return ordersService.createOrder(order);
-	}
-	
-	// 주문 목록 조회 
-	@PostMapping("/getorderlist")
-	public List<Orders> getOrdersList(@RequestParam("userId") Long userId) {
-	    return ordersService.getOrdersListByUserId(userId);
-	}
+    OrdersService ordersService;
 
-	// 다른 엔드포인트들도 유사한 방식으로 엔티티를 사용하여 구현
+
+// ** Cart List - 회원별 카트 목록 반환 
+    @GetMapping("/ordersList")
+    public void ordersList(Model model) {
+    	model.addAttribute("myorders", ordersService.selectList());
+    } // cartList
+
+
+
+	// ** Order Cancel - 주문 취소
+	@GetMapping(value="/ocancel") // ocancel => 주문취소(oredercancel)
+	public String ocancel(HttpSession session, Orders entity, RedirectAttributes rttr) {
+		
+		String uri = "redirect:/orders/ordersList";
+		
+		try {
+			log.info("** cancel 성공  => "+ordersService.delete(entity.getOrder_num()));
+			rttr.addFlashAttribute("message", "~~ 주문취소 성공!! ~~") ;	
+			if ( ((String)session.getAttribute("loginID")).equals("admin") ) {
+				// => 관리자에 의한 강제 주문취소 : orderList.jsp
+				uri="redirect:ordersList";
+			}else {
+				// => 본인삭제 : home.jsp, session 무효화 -> ??????????
+				session.invalidate();
+			}
+		} catch (Exception e) {
+			log.info("** delete Exception => "+e.toString());
+			rttr.addFlashAttribute("message", "~~ 취소 실패 ~~");
+		}
+		
+		return uri;
+	} 
 }
+
+
 
