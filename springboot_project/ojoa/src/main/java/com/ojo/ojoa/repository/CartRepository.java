36142c7ae2980b2198ca0b98b57de42306aa267b
DESKTOP-PARGD12\Wonhee <lehn3599@naver.com>
2023-11-30 17:35:25 +0900

    테이블조인성공

--- springboot_project/ojoa/src/main/java/com/ojo/ojoa/repository/CartRepository.java
+++ springboot_project/ojoa/src/main/java/com/ojo/ojoa/repository/CartRepository.java
@@ -3,11 +3,16 @@ package com.ojo.ojoa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.ojo.ojoa.domain.CartProdDTO;
 import com.ojo.ojoa.entity.Cart;
 
 public interface CartRepository 
 extends JpaRepository<Cart, Integer> {
 
-
+// 장바구니 + 상품 Join
+@Query("SELECT new com.ojo.ojoa.domain.CartProdDTO(c.cart_num, c.id, c.prod_num, c.quantity, p.prod_mainimage, p.prod_name) "
+		+ "FROM Cart c LEFT JOIN Product p ON c.prod_num=p.prod_num order by c.prod_num")
+List<CartProdDTO> findCartProd();
 }
